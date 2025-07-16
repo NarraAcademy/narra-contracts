210e5f76a2579d6d7e53570de5683579d683e45d
nick.btc <260442518@qq.com>
2025-07-16 17:48:01 +0800

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  paths: {
+    sources: "src",
+    tests: "test",
+    cache: "cache",
+    artifacts: "artifacts",
+  },
+};
+
+export default config;
